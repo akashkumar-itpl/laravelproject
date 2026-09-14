cd14458321504b3d463e86b7d02725c6ca06bc16
akashkumar-itpl <akash.kumar@hkdigitalonline.com>
2026-09-14 18:43:04 +0530

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -55,7 +55,7 @@ class HomeController extends Controller
 
     {
 
-        echo "its m"; die;
+      //  echo "its m"; die;
 
         $homeData         = HomePage::all();
         $aboutdata = AboutUs::all();
